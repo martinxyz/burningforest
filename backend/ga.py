a93610a8d035ff8ad9a559f9d56c37385e148cfb
Martin Renold <martin@log2.ch>
2018-01-04 18:35:38 +0100

    basic ga learning to make everything white

--- backend/ga.py
+++ backend/ga.py
@@ -2,23 +2,17 @@
 
 import render
 import random
-import numpy
-import numpy.random
-import scipy.misc
+import numpy as np
+# import numpy.random
+import matplotlib.pyplot as plt
 import math
+import dirichlet  # https://github.com/ericsuh/dirichlet
 
 symbols = 'SJKLA+-[]\t'  # \t for end-of-sequence
 
-symbol_a = numpy.ones(len(symbols))
-# create slight bias towards equal probability of all symbols
-symbol_a *= 2
-
-pvals = numpy.random.dirichlet(symbol_a)
-
-
 def sample_system(pvals):
     def sample_rule(pvals):
-        symlist = numpy.random.choice(list(symbols), 80, p=pvals)
+        symlist = np.random.choice(list(symbols), 80, p=pvals)
         rule = ''.join(symlist).split('\t')[0]
         return rule
 
@@ -40,7 +34,73 @@ def sample_system(pvals):
     return system
 
 
-for i in range(80):
-    system = sample_system(pvals)
+def evaluate_system(system):
     buf = render.render(system)
-    scipy.misc.imsave('ga-test-%04d.png' % i, buf)
+    loss = -buf.mean()
+    return loss, buf
+
+
+def evaluate_pvals(system):
+    best_loss = None
+    best_buf = None
+    for i in range(5):
+        system = sample_system(pvals)
+        loss, buf = evaluate_system(system)
+        if best_loss is None or loss < best_loss:
+            best_loss = loss
+            best_buf = buf
+    return best_loss, best_buf
+
+
+if __name__ == '__main__':
+    iterations = 200
+
+    best_factor = 0.01
+    evaluations = 1000
+
+    symbol_a = np.ones(len(symbols))
+    # sentinel_a = np.ones(10)
+    # create slight bias towards equal probability of all symbols
+    symbol_a *= 2
+
+    for it in range(iterations):
+        print('iteration', it)
+        sample = []
+        loss_best = None
+        res_best = None
+        for i in range(evaluations):
+            pvals = np.random.dirichlet(symbol_a)
+            # pvals_sentinel = np.random.dirichlet(sentinel_a)
+            loss, buf = evaluate_pvals(pvals)
+            sample.append((loss, pvals, buf))
+
+        sample.sort(key=lambda s: s[0])
+        sample_loss = np.array([s[0] for s in sample])
+        sample_pvals = np.array([s[1] for s in sample])
+        sample_buf = np.array([s[2] for s in sample])
+
+        res_best = sample_buf[1]
+
+        print('mean loss was', sample_loss.mean())
+        print('min loss was', sample_loss.min())
+        plt.ion()
+        plt.figure(1)
+        plt.clf()
+        plt.plot(sample_loss)
+        plt.draw()
+        # plt.show()
+        plt.pause(0.1)
+
+        # estimate new dirichlet distribution
+        new_symbol_a = dirichlet.mle(sample_pvals[:int(evaluations*best_factor), :])
+        print('old symbol_a:', symbol_a)
+        print('new symbol_a:', new_symbol_a)
+        # symbol_a = 0.5 * symbol_a + 0.5 * new_symbol_a
+        symbol_a = new_symbol_a
+
+        plt.figure(2)
+        plt.clf()
+        plt.imshow(res_best)
+        plt.gray()
+        plt.draw()
+        plt.pause(0.1)
